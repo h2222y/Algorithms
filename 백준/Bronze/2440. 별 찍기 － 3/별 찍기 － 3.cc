033aefdc9615ce6c441c0c6e92f5322acee7801a
김혜수 <76990529+h2222y@users.noreply.github.com>
2022-09-06 23:43:10 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for (int x = 0; x < n; x++) {
+		for (int y = 0; y < n - x; y++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
