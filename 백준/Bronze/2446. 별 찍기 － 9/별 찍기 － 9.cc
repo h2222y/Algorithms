aeefca24faaa3f3f906a2099d6a2e5eaada0e263
김혜수 <76990529+h2222y@users.noreply.github.com>
2022-09-07 00:10:50 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for (int x = 0; x < n; x++) {
+		for (int y = 0; y < x; y++) {
+			cout << " ";
+		}
+		for (int y = 0; y < 2*n - 2*x-1; y++) {
+			cout << "*";
+		}
+
+		cout << "\n";
+	}
+	for (int x = 0; x < n - 1; x++) {
+		for (int y = 0; y < n - x -2; y++) {
+			cout << " ";
+		}
+		for (int y = 0; y < 2*x +3; y++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
