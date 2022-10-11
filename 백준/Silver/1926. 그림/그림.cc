ce77439b97aed0e51e2b722a3155b321dfb99a8e
김혜수 <76990529+h2222y@users.noreply.github.com>
2022-10-12 01:25:34 +0900

    [Silver I] Title: 그림, Time: 56 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,32 +1,33 @@
 #include <iostream>
 #include <queue>
 #include <utility>
-using namespace std;
 #define X first
 #define Y second
+using namespace std;
 
-int map[501][501];
+int board[501][501];
 int vis[501][501];
-queue<pair<int,int>> Q;
 int dx[4] = { 0,0,-1,1 };
 int dy[4] = { -1,1,0,0 };
+queue<pair<int, int>> Q;
 
 int main() {
 	int n, m;
 	cin >> n >> m;
+
 	for (int x = 0; x < n; x++) {
 		for (int y = 0; y < m; y++) {
-			cin >> map[x][y];
+			cin >> board[x][y];
 		}
 	}
-	// 0은 색칠이 안된부분, 1은 색칠이 된 부분
 
-	int cnt = 0;
 	int width = 0;
-	int mx = 0;
+	int cnt = 0;
+	int max_width = 0;
+
 	for (int x = 0; x < n; x++) {
 		for (int y = 0; y < m; y++) {
-			if (map[x][y] == 0 || vis[x][y] == 1) continue;
+			if (board[x][y] == 0 || vis[x][y] == 1)continue;
 			cnt++;
 			vis[x][y] = 1;
 			Q.push({ x,y });
@@ -35,21 +36,19 @@ int main() {
 			while (!Q.empty()) {
 				pair<int, int> cur = Q.front();
 				Q.pop();
-					for (int i = 0; i < 4; i++) {
-						int nx = cur.X + dx[i];
-						int ny = cur.Y + dy[i];
-						if (nx < 0 || nx >= n || ny<0 || ny>=m)continue;
-						if (vis[nx][ny] == 1 || map[nx][ny] == 0)continue;
-						vis[nx][ny] = 1;
-						Q.push({ nx,ny });
-						width++;
-					}
-				mx = max(mx, width);
+				for (int t = 0; t < 4; t++) {
+					int nx = cur.X + dx[t];
+					int ny = cur.Y + dy[t];
+					if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+					if (vis[nx][ny] == 1 || board[nx][ny] == 0) continue;
+					vis[nx][ny] = 1;
+					Q.push({ nx,ny });
+					width++;
+				}
+				max_width = max(max_width, width);
 			}
 		}
 	}
-	cout << cnt << "\n"<<mx;
-
-	
-	return 0; 
+	cout << cnt << "\n" << max_width;
+	return 0;
 }
