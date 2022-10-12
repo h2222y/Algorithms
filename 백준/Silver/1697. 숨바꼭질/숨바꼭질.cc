5f720ebf37ff7eb84de7a5902839b97de1be3822
김혜수 <76990529+h2222y@users.noreply.github.com>
2022-10-12 14:09:00 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -2,25 +2,25 @@
 #include <queue>
 using namespace std;
 
-int vis[100002]; // 수빈이가 이동하는 배열
+int vis[100001];
+queue <int> Q;
 
 int main() {
 	int n, k;
-	cin >> n >> k; //수빈이의 좌표와 동생의 좌표 입력
-	queue<int> Q;
-	Q.push(n); // 수빈이의 처음 좌표를 큐에 넣는다
-	fill(vis, vis + 100001, -1); // 이동 배열을 -1로 초기화
-	vis[n] = 0; // 수빈이의 첫 이동좌표에 0을 넣어준다. 
-	while (vis[k] == -1) { // -1일동안
-		int cur = Q.front();
+	cin >> n >> k; // subin location, sister location
+	fill(vis, vis + 100001, -1); // initialization visit array
+	Q.push(n); // push subin's location
+	vis[n] = 0; // start with 0 
+	while (vis[k] == -1) { // when not visit sister's location
+		int cur = Q.front(); // current location
 		Q.pop();
-		for (int nxt : {cur - 1, cur + 1, 2 * cur}) { //이동은 cur-1,cur+1,cur*2 가능
-			if (nxt < 0 || nxt>100000)continue; // nxt의 값이 범위를 벗어날때
-			if (vis[nxt] != -1) continue; // 이미 방문했을때
-			vis[nxt] = vis[cur] + 1; // 현재좌표 +1을 넣어준다
+		for (int nxt : {cur - 1, cur + 1, cur * 2}) { // move
+			if (nxt < 0 || nxt>100000) continue; // out of range
+			if (vis[nxt] != -1) continue; // already visit
+			vis[nxt] = vis[cur] + 1; // count seconds
 			Q.push(nxt);
 		}
 	}
-	cout << vis[k]; //동생의 좌표에 있는 이동 좌표를 출력해준다.
+	cout << vis[k]; 
 	return 0;
 }
