78ee5af6d79acaf837e0dbd85cad9c33b5e6895e
김혜수 <76990529+h2222y@users.noreply.github.com>
2022-11-01 20:53:29 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int a, b;
+	while (1) {
+		cin >> a >> b;
+		if (a == 0 && b == 0) return 0;
+		if (a > b) cout << "Yes\n";
+		else cout << "No\n";
+	}
+
+	return 0; 
+}
