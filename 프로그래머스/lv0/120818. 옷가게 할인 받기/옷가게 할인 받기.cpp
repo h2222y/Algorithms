35adc319e9f56f0ef45035e3ef378a5c5ce2329e
김혜수 <76990529+h2222y@users.noreply.github.com>
2022-11-09 21:41:05 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int price) {
+    int answer = 0;
+    double discount=0;
+    if(price>=500000){
+        discount = price*0.2;
+    }
+    else if(price>=300000){
+        discount = price*0.1;
+    }
+    else if(price>=100000){
+        discount = price*0.05;
+    }
+    answer = price- discount;
+    return answer;
+}
