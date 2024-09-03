bace45b442ba4f3d52c01aeced2912a833b1773b
김혜수 <76990529+h2222y@users.noreply.github.com>
2024-09-04 00:04:05 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -2,27 +2,24 @@
 #include <string>
 using namespace std;
 
-int cnt[10];
-
-int main() {
+int main()
+{
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+
 	int a, b, c;
 	cin >> a >> b >> c;
+	int mul = a * b * c;
 	
-	int multi = a * b * c;
-	
-	string str_multi;
-	str_multi = to_string(multi);
-
-	for (int i = 0; i < str_multi.length(); i++) {
-		cnt[str_multi[i] - '0']++;
+	string strMul = to_string(mul);
+	int dat[20] = {};
+	for (int i = 0; i < strMul.length(); i++)
+	{
+		dat[strMul[i] - '0']++;
 	}
-
-	for (int i = 0; i < 10; i++) {
-		cout << cnt[i] << "\n";
+	for (int i = 0; i <= 9; i++)
+	{
+		cout << dat[i] << "\n";
 	}
-
-
 	return 0;
 }
